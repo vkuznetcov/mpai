e0c0a886f937caf9fc6b2b02e8c5883c4fa04dea
slav <vkuznetcov48@gmail.com>
2022-02-09 16:09:26 +0400

    renamed name of result file

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -9,7 +9,7 @@ b = float(input('input B parameter: '))
 original = plt.imread('lab1/cowboi.jpg')
 grayscale = np.dot(original, [r, g, b])
 
-plt.imsave('lab1/cowboi_gray.jpg', grayscale)
+plt.imsave('lab1/cowboi_polutone.jpg', grayscale)
 
 fig, axes = plt.subplots(1, 2, figsize = (10, 5))
 ax = axes.ravel()
